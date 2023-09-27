cbd647b4e2a422e2855f7e52f8833d680502dfda
The Real Mi <miaolmedillo@gmail.com>
2023-09-26 21:01:22 -0400

    added some seeds and established model relationships

--- models/index.js
+++ models/index.js
@@ -1,1 +1,34 @@
 //establish relationships here
+const User = require('./User');
+const Pet = require('./Pet');
+const Behavior = require('./Behavior');
+const Session = require('./Session');
+
+User.hasMany(Pet, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Pet.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Pet.hasMany(Behavior, {
+    foreignKey: 'pet_id',
+    onDelete: 'CASCADE'
+});
+
+Behavior.belongsTo(Pet, {
+    foreignKey: 'pet_id'
+});
+
+Behavior.hasMany(Session, {
+    foreignKey: 'behavior_id',
+    onDelete: 'CASCADE'
+});
+
+Session.belongsTo(Behavior, {
+    foreignKey: 'behavior_id'
+});
+
+module.exports = { User, Pet, Behavior, Session };
