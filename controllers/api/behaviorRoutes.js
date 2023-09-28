793955ff66d8baf09777badbda168e60cfc945b0
The Real Mi <miaolmedillo@gmail.com>
2023-09-28 12:20:28 -0400

    added route to create a new training log

--- controllers/api/behaviorRoutes.js
+++ controllers/api/behaviorRoutes.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
-const { Pet, Behavior } = require('../../models');
+const { Pet, Behavior, User, Training } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-// Route to get all behaviors
-router.get('/', async (req, res) => {
+// Route to get all behaviors for a specific pet
+router.get('/:pet_id', async (req, res) => {
     try {
         const behaviors = await Behavior.findAll({
-            include: [{ model: Pet }]
+            include: [{ model: Pet }, { model: User }],
+            where: {
+                user_id: req.session.user_id,
+                pet_id: req.params.pet_id
+            }
         });
         res.status(200).json(behaviors);
     } catch (err) {
@@ -13,8 +18,8 @@ router.get('/', async (req, res) => {
     }
 });
 
-// Route to create a new behavior
-router.post('/', async (req, res) => {
+// Route to create a new behavior for a specific pet
+router.post('/', withAuth, async (req, res) => {
     try {
         const newBehavior = await Behavior.create(req.body);
         res.status(201).json(newBehavior);
@@ -22,3 +27,17 @@ router.post('/', async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+// Route to create a new training log 
+router.post('/:behavior_id', withAuth, async (req, res) => {
+    try {
+      const newTraining = await Training.create({
+        ...req.body,
+        behavior_id: req.params.behavior_id
+      });
+  
+      res.status(200).json(newTraining);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
